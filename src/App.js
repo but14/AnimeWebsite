83deceb4078d7827a533dcde4e29bcd4589a763c
Longngoo <100961248+but14@users.noreply.github.com>
2024-07-31 15:12:17 +0700

    code commit 3107

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Popular from "./Components/Popular";
 import AnimeItem from "./Components/AnimeItem";
+import Gallery from "./Components/Gallery";
+import Homepage from "./Components/Homepage";
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Popular />} />
-        <Route path="/anime/:id" element={<AnimeItem/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
   );
